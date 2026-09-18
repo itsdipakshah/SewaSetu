e55e2f49166c645fe24182552b3ee5a486d337be
dipak-kr <dipakshah3321@gmail.com>
2026-09-18 11:33:17 +0545

    free shipping box done

--- Client/src/components/catSlider/HomeCatSlider.jsx
+++ Client/src/components/catSlider/HomeCatSlider.jsx
@@ -1,23 +1,27 @@
-import React, { useRef, useState } from "react";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 
 
 const HomeCatSlider = () => {
   return (
-    <>
+    <div className="!py-6 !pt-4">
       <Swiper
         slidesPerView={7}
         spaceBetween={30}
         pagination={{
-          clickable: true,
+          clickable: false,
         }}
-
+        navigation={true}
+        modules={Navigation}
         className="mySwiper w-[95%] !h-[100%] mx-auto !my-6 "
       >
         <SwiperSlide>
-          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-[rgba(42,41,41,0.34)] hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
+          
+          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-[rgba(94,85,85,0.34)] hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
             <img
               src="https://png.pngtree.com/png-vector/20250703/ourmid/pngtree-black-headphones-sleek-3d-render-png-image_16600605.webp"
               alt="creatine"
@@ -27,7 +31,7 @@ const HomeCatSlider = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-black hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
+          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-[rgba(94,85,85,0.34)] hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
             <img
               src="https://png.pngtree.com/png-vector/20250608/ourmid/pngtree-stacked-folded-blue-jeans-png-image_16476863.png"
               alt="mobile"
@@ -37,7 +41,7 @@ const HomeCatSlider = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="item text-center justify-center rounded-md flex flex-col bg-white">
+          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-[rgba(94,85,85,0.34)] hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
             <img
               src="https://png.pngtree.com/png-vector/20241205/ourmid/pngtree-beauty-parlour-cosmetics-products-transparent-background-image-png-image_14646730.png"
               alt="mobile"
@@ -47,7 +51,7 @@ const HomeCatSlider = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="item text-center justify-center rounded-md flex flex-col bg-white">
+          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-[rgba(94,85,85,0.34)] hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
             <img
               src="https://png.pngtree.com/png-vector/20240807/ourmid/pngtree-juicy-fruits-and-vitamins-natural-organic-fruits-png-image_13146415.png"
               alt="mobile"
@@ -58,7 +62,7 @@ const HomeCatSlider = () => {
         </SwiperSlide>
         
         <SwiperSlide>
-          <div className="item text-center justify-center rounded-md flex flex-col bg-white">
+          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-[rgba(94,85,85,0.34)] hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
             <img
               src="https://static.vecteezy.com/system/resources/previews/054/044/054/non_2x/medical-red-first-aid-kit-with-medicine-on-transparent-background-free-png.png"
               alt="mobile"
@@ -68,7 +72,7 @@ const HomeCatSlider = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="item text-center justify-center rounded-md flex flex-col bg-white">
+          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-[rgba(94,85,85,0.34)] hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
             <img
               src="https://png.pngtree.com/png-clipart/20250415/original/pngtree-indian-gold-jewellery-set-png-image_20711595.png"
               alt="mobile"
@@ -78,7 +82,7 @@ const HomeCatSlider = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="item text-center justify-center rounded-md flex flex-col bg-white">
+          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-[rgba(94,85,85,0.34)] hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
             <img
               src="https://static.vecteezy.com/system/resources/thumbnails/050/704/376/small/dirt-bike-helmet-isolated-on-transparent-background-png.png"
               alt="mobile"
@@ -89,7 +93,7 @@ const HomeCatSlider = () => {
         </SwiperSlide>
         
         <SwiperSlide>
-          <div className="item text-center justify-center rounded-md flex flex-col bg-white">
+          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-[rgba(94,85,85,0.34)] hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRChJ7EpKkHWUONG1wXMNxvYo_TpphZvxDIb_0TXqnv_A&s=10"
               alt="mobile"
@@ -99,7 +103,7 @@ const HomeCatSlider = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="item text-center justify-center rounded-md flex flex-col bg-white">
+          <div className="item text-center justify-center rounded-md flex flex-col bg-white border border-transparent transition-all duration-300 hover:border-[rgba(94,85,85,0.34)] hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] cursor-pointer">
             <img
               src="https://static.vecteezy.com/system/resources/thumbnails/052/646/401/small/vibrant-stacked-silk-sarees-in-hues-isolated-transparent-png.png"
               alt="mobile"
@@ -111,7 +115,7 @@ const HomeCatSlider = () => {
         
         
       </Swiper>
-    </>
+    </div>
   );
 };
 
--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomeSlider from '../../components/slider/HomeSlider'
 import HomeCatSlider from '../../components/catSlider/HomeCatSlider'
+import { FaShippingFast } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -8,10 +9,34 @@ const Home = () => {
     <HomeSlider/>
     <HomeCatSlider/>
 
+    <section className='!py-16 !bg-white'>
+      
+        <div className="container">
+          <div className="freeshipping w-full border-2 !p-4 rounded-sm border-[#ff5252] flex items-center justify-between">
+            <div className="col1 flex items-center gap-4">
+              <FaShippingFast className='text-[50px]' />
+              <span className='text-[25px] font-[600] uppercase'> Free Shipping</span>
+            </div>
+
+            <div className="cols2">
+              <p className='font-[500]'>Free delivery now on your first order and over 200$</p>
+            </div>
+
+            <div className="cols3">
+              <p className='text-[25px] font-[600] uppercase'> -Only 200$* </p>
+            </div>
+
+
+          </div>
+        </div>
+     
+    </section>
+
     <br/>
     <br/>
     <br/>
     <br/>
+
     </>
   )
 }
